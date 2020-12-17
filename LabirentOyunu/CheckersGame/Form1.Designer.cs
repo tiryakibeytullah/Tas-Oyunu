8410a34bdf81a5b40c0cf42b626ae28571d66d3e
Beytullah Tiryaki <tiryakibeytullah10@gmail.com>
2020-12-17 16:54:51 +0300

    Labirent Oyunu
    C# ile yapılmış olan, yeşil taşların engelleri geçerek kırmızı taşa ulaşılması amacıyla yapılmış bir mini oyun.

---
+++ LabirentOyunu/CheckersGame/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace CheckersGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.G = new System.Windows.Forms.Panel();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.ımageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.lblAnaTasSayisi = new System.Windows.Forms.Label();
+            this.lblDuvarSayisi = new System.Windows.Forms.Label();
+            this.lblYesilTasSayisi = new System.Windows.Forms.Label();
+            this.btnAnaTas = new System.Windows.Forms.Button();
+            this.btnYesilTas = new System.Windows.Forms.Button();
+            this.btnDuvarEkle = new System.Windows.Forms.Button();
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // G
+            // 
+            this.G.BackColor = System.Drawing.Color.Gray;
+            this.G.Location = new System.Drawing.Point(10, 10);
+            this.G.Name = "G";
+            this.G.Size = new System.Drawing.Size(484, 484);
+            this.G.TabIndex = 0;
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "b.png");
+            this.ımageList1.Images.SetKeyName(1, "d.png");
+            this.ımageList1.Images.SetKeyName(2, "g.png");
+            this.ımageList1.Images.SetKeyName(3, "r.png");
+            // 
+            // ımageList2
+            // 
+            this.ımageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList2.ImageStream")));
+            this.ımageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList2.Images.SetKeyName(0, "b.png");
+            this.ımageList2.Images.SetKeyName(1, "d.png");
+            this.ımageList2.Images.SetKeyName(2, "g.png");
+            this.ımageList2.Images.SetKeyName(3, "r.png");
+            // 
+            // lblAnaTasSayisi
+            // 
+            this.lblAnaTasSayisi.AutoSize = true;
+            this.lblAnaTasSayisi.BackColor = System.Drawing.Color.Transparent;
+            this.lblAnaTasSayisi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnaTasSayisi.ForeColor = System.Drawing.Color.White;
+            this.lblAnaTasSayisi.Location = new System.Drawing.Point(550, 70);
+            this.lblAnaTasSayisi.Name = "lblAnaTasSayisi";
+            this.lblAnaTasSayisi.Size = new System.Drawing.Size(0, 24);
+            this.lblAnaTasSayisi.TabIndex = 4;
+            // 
+            // lblDuvarSayisi
+            // 
+            this.lblDuvarSayisi.AutoSize = true;
+            this.lblDuvarSayisi.BackColor = System.Drawing.Color.Transparent;
+            this.lblDuvarSayisi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDuvarSayisi.ForeColor = System.Drawing.Color.White;
+            this.lblDuvarSayisi.Location = new System.Drawing.Point(657, 70);
+            this.lblDuvarSayisi.Name = "lblDuvarSayisi";
+            this.lblDuvarSayisi.Size = new System.Drawing.Size(0, 24);
+            this.lblDuvarSayisi.TabIndex = 5;
+            // 
+            // lblYesilTasSayisi
+            // 
+            this.lblYesilTasSayisi.AutoSize = true;
+            this.lblYesilTasSayisi.BackColor = System.Drawing.Color.Transparent;
+            this.lblYesilTasSayisi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYesilTasSayisi.ForeColor = System.Drawing.Color.White;
+            this.lblYesilTasSayisi.Location = new System.Drawing.Point(757, 70);
+            this.lblYesilTasSayisi.Name = "lblYesilTasSayisi";
+            this.lblYesilTasSayisi.Size = new System.Drawing.Size(0, 24);
+            this.lblYesilTasSayisi.TabIndex = 6;
+            // 
+            // btnAnaTas
+            // 
+            this.btnAnaTas.BackColor = System.Drawing.Color.Transparent;
+            this.btnAnaTas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnAnaTas.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnaTas.ForeColor = System.Drawing.Color.Black;
+            this.btnAnaTas.ImageIndex = 3;
+            this.btnAnaTas.ImageList = this.ımageList1;
+            this.btnAnaTas.Location = new System.Drawing.Point(512, 96);
+            this.btnAnaTas.Name = "btnAnaTas";
+            this.btnAnaTas.Size = new System.Drawing.Size(98, 67);
+            this.btnAnaTas.TabIndex = 3;
+            this.btnAnaTas.Text = "Ana Taş Ekle";
+            this.btnAnaTas.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAnaTas.UseVisualStyleBackColor = false;
+            this.btnAnaTas.Click += new System.EventHandler(this.btnAnaTas_Click);
+            // 
+            // btnYesilTas
+            // 
+            this.btnYesilTas.BackColor = System.Drawing.Color.Transparent;
+            this.btnYesilTas.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnYesilTas.ForeColor = System.Drawing.Color.Black;
+            this.btnYesilTas.ImageIndex = 2;
+            this.btnYesilTas.ImageList = this.ımageList1;
+            this.btnYesilTas.Location = new System.Drawing.Point(718, 96);
+            this.btnYesilTas.Name = "btnYesilTas";
+            this.btnYesilTas.Size = new System.Drawing.Size(99, 67);
+            this.btnYesilTas.TabIndex = 2;
+            this.btnYesilTas.Text = "Yeşil Taş Ekle";
+            this.btnYesilTas.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnYesilTas.UseVisualStyleBackColor = false;
+            this.btnYesilTas.Click += new System.EventHandler(this.btnYesilTas_Click);
+            // 
+            // btnDuvarEkle
+            // 
+            this.btnDuvarEkle.BackColor = System.Drawing.Color.Transparent;
+            this.btnDuvarEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDuvarEkle.ForeColor = System.Drawing.Color.Black;
+            this.btnDuvarEkle.ImageIndex = 1;
+            this.btnDuvarEkle.ImageList = this.ımageList2;
+            this.btnDuvarEkle.Location = new System.Drawing.Point(616, 96);
+            this.btnDuvarEkle.Name = "btnDuvarEkle";
+            this.btnDuvarEkle.Size = new System.Drawing.Size(96, 67);
+            this.btnDuvarEkle.TabIndex = 1;
+            this.btnDuvarEkle.Text = "Duvar Ekle";
+            this.btnDuvarEkle.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnDuvarEkle.UseVisualStyleBackColor = false;
+            this.btnDuvarEkle.Click += new System.EventHandler(this.btnDuvarEkle_Click);
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.BackColor = System.Drawing.Color.Transparent;
+            this.lblLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLevel.ForeColor = System.Drawing.Color.White;
+            this.lblLevel.Location = new System.Drawing.Point(624, 10);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(0, 31);
+            this.lblLevel.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(764, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(515, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(253, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Seçili taşın Ana taşa uzaklığı : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CheckersGame.Properties.Resources.wallpaper2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(859, 518);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.lblYesilTasSayisi);
+            this.Controls.Add(this.lblDuvarSayisi);
+            this.Controls.Add(this.lblAnaTasSayisi);
+            this.Controls.Add(this.btnAnaTas);
+            this.Controls.Add(this.btnYesilTas);
+            this.Controls.Add(this.btnDuvarEkle);
+            this.Controls.Add(this.G);
+            this.ForeColor = System.Drawing.Color.Red;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CheckersGame";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel G;
+        private System.Windows.Forms.Button btnDuvarEkle;
+        private System.Windows.Forms.Button btnYesilTas;
+        private System.Windows.Forms.Button btnAnaTas;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.ImageList ımageList2;
+        private System.Windows.Forms.Label lblAnaTasSayisi;
+        private System.Windows.Forms.Label lblDuvarSayisi;
+        private System.Windows.Forms.Label lblYesilTasSayisi;
+        private System.Windows.Forms.Label lblLevel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LabirentOyunu/CheckersGame/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckersGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int n,sayac = 0;
+        int Kordinat_x, Kordinat_y;
+        int geciciKirmizi_x, geciciKirmizi_y, geciciYesil_x, geciciYesil_y;
+        PictureBox[,] P;
+        Random rnd = new Random();
+        string color = "g", k = " ", B1 = " ", B2 = " ", B3 = " ", B4 = " ";
+        int level = 1;
+        int anaTas = 0, yesilTas = 0, duvar = 0;
+
+        private void btnAnaTas_Click(object sender, EventArgs e)
+        {
+            Kordinat_x = rnd.Next(2, 7);
+            Kordinat_y = rnd.Next(2, 7);
+
+            if ((P[Kordinat_x, Kordinat_y].Image == null) && (anaTas > 0))
+            {
+                P[Kordinat_x, Kordinat_y].Image = Properties.Resources.r;
+                P[Kordinat_x, Kordinat_y].Name += " r";
+                geciciKirmizi_x = Kordinat_x;
+                geciciKirmizi_y = Kordinat_y;
+                anaTas -= 1;
+
+                if (anaTas == 0) { btnAnaTas.Enabled = false; }
+            }
+
+            lblAnaTasSayisi.Text = anaTas.ToString();
+        }
+
+        private void btnYesilTas_Click(object sender, EventArgs e)
+        {
+            Kordinat_x = rnd.Next(1, 8);
+            Kordinat_y = rnd.Next(1, 8);
+
+            if ((P[Kordinat_x, Kordinat_y].Image == null) && (yesilTas > 0))
+            {
+                P[Kordinat_x, Kordinat_y].Image = Properties.Resources.g;
+                P[Kordinat_x, Kordinat_y].Name += " g";
+                yesilTas -= 1;
+
+                if (yesilTas == 0) { btnYesilTas.Enabled = false; }
+            }
+
+            lblYesilTasSayisi.Text = yesilTas.ToString();
+        }
+
+        private void btnDuvarEkle_Click(object sender, EventArgs e)
+        {
+            Kordinat_x = rnd.Next(1, 8);
+            Kordinat_y = rnd.Next(1, 8);
+
+            if ((P[Kordinat_x, Kordinat_y].Image == null) && (duvar > 0))
+            {
+                P[Kordinat_x, Kordinat_y].Image = Properties.Resources.d;
+                P[Kordinat_x, Kordinat_y].Name += " d";
+                duvar -= 1;
+
+                if (duvar == 0) { btnDuvarEkle.Enabled = false; }
+            }
+            lblDuvarSayisi.Text = duvar.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Oyun();
+        }
+        private void Oyun()
+        {
+            switch (level)
+            {
+                case 1:
+                    lblLevel.Text = "Level 1";
+                    anaTas = 1; yesilTas = 7; duvar = 5;
+                    break;
+
+                case 2:
+                    lblLevel.Text = "Level 2";
+                    anaTas = 1; yesilTas = 8; duvar = 5;
+                    break;
+
+                default:
+                    break;
+            }
+
+            lblAnaTasSayisi.Text = anaTas.ToString();
+            lblYesilTasSayisi.Text = yesilTas.ToString();
+            lblDuvarSayisi.Text = duvar.ToString();
+
+            // Tahtayı oluşturduğumuz alan.
+
+            n = 8;
+            P = new PictureBox[n, n];
+            int left = 2, top = 2;
+            Color[] colors = new Color[] { Color.White, Color.Black };
+            for (int i = 0; i < n; i++)
+            {
+                left = 2;
+                if (i % 2 == 0) { colors[0] = Color.White; colors[1] = Color.Black; }
+                else { colors[0] = Color.Black; colors[1] = Color.White; }
+
+                for (int j = 0; j < n; j++)
+                {
+                    P[i, j] = new PictureBox();
+                    P[i, j].BackColor = colors[(j % 2 == 0) ? 1 : 0];
+                    P[i, j].Location = new Point(left, top);
+                    P[i, j].Size = new Size(60, 60);
+                    left += 60;
+
+                    // Her bir Kare'ye konumlandırmak için adlarını i + j olarak verdik.
+
+                    P[i, j].Name = i + " " + j;
+
+                    P[i, j].SizeMode = PictureBoxSizeMode.CenterImage;
+
+                    P[i, j].Click += (sender3, e3) =>
+                    {
+
+                        PictureBox p = sender3 as PictureBox;
+                        if (p.Image != null)
+                        {
+                            int c = 0, x, y;
+
+                            #region Taşları sürüklemek için burayı kullandık.
+                            F();
+
+                            // Mavi taşa tıklanırsa olucaklar (Yeşil taşları sürükleme olayı) : 
+                            if (p.Name.Split(' ')[2] == "b")
+                            {
+                                if (color == "r") color = "g";
+                                else color = "g";
+                                x = Convert.ToInt32(k.Split(' ')[0]);
+                                y = Convert.ToInt32(k.Split(' ')[1]);
+                                B1 = " ";
+                                B2 = " ";
+                                B3 = " ";
+                                B4 = " ";
+                                if (k.Split(' ')[2] == "r")
+                                {
+                                    p.Image = Properties.Resources.r;
+                                    p.Name = p.Name.Replace("b", "r");
+                                }
+                                if (k.Split(' ')[2] == "g")
+                                {
+                                    p.Image = Properties.Resources.g;
+                                    p.Name = p.Name.Replace("b", "g");
+                                }
+                                P[x, y].Image = null;
+                            }
+                            // Kırmızı taşa tıklanırsa olucaklar :
+                            if (p.Name.Split(' ')[2] == "r")
+                            {
+                                x = Convert.ToInt32(k.Split(' ')[0]);
+                                y = Convert.ToInt32(k.Split(' ')[1]);
+
+                                if ((P[x + c, y + 1].Name.Split(' ')[2] == "r") || (P[x, y - 1].Name.Split(' ')[2] == "r") || (P[x + 1, y].Name.Split(' ')[2] == "r") || (P[x - 1, y].Name.Split(' ')[2] == "r"))
+                                {
+                                    P[x, y].Image = null;
+                                    sayac += 1;
+
+                                    // Level'lere göre olucak işlemler.
+                                    if (sayac == 8)
+                                    {
+                                        MessageBox.Show("Tebrikler oyunu bitirdiniz.");
+                                        this.Close();
+                                    }
+                                    else if ((sayac == 7) && (level == 1))
+                                    {
+                                        level += 1;
+                                        MessageBox.Show("Tebrikler kazandınız. Şimdi Level 2'den devam edin..");
+                                        btnAnaTas.Enabled = true;
+                                        btnYesilTas.Enabled = true;
+                                        btnDuvarEkle.Enabled = true;
+
+                                        for (int k = 0; k < 8; k++)
+                                        {
+                                            for (int l = 0; l < 8; l++)
+                                            {
+                                                P[k, l].Image = null;
+                                            }
+                                        }
+                                        G.Controls.Clear();
+                                        sayac = 0;
+                                        Oyun();
+                                    }
+                                }
+                            }
+
+                            #endregion
+
+                            #region Tıklanılan taşın 4 tarafında mavi alan çıkarttık.
+                            if (p.Name.Split(' ')[2] == color)
+                            {
+                                
+                                x = Convert.ToInt32(p.Name.Split(' ')[0]);
+                                y = Convert.ToInt32(p.Name.Split(' ')[1]);
+                                k = p.Name;
+
+                                geciciYesil_x = x;
+                                geciciYesil_y = y;
+
+                                int tasUstundekiSayi = (Math.Abs(geciciYesil_x - geciciKirmizi_x) + Math.Abs(geciciYesil_y - geciciKirmizi_y)) - 1;
+                                label1.Text = Math.Abs(tasUstundekiSayi).ToString();
+
+                                if (p.Name.Split(' ')[2] == "r") c = 0;
+
+                                try
+                                {
+                                    if (P[x + c, y + 1].Image == null)
+                                    {
+                                        P[x + c, y + 1].Image = Properties.Resources.b;
+                                        P[x + c, y + 1].Name = (x + c) + " " + (y + 1) + " b";
+                                        B1 = (x + c) + " " + (y + 1);
+                                    }
+                                }
+                                catch { }
+
+                                try
+                                {
+                                    if (P[x + c, y - 1].Image == null)
+                                    {
+                                        P[x + c, y - 1].Image = Properties.Resources.b;
+                                        P[x + c, y - 1].Name = (x + c) + " " + (y - 1) + " b";
+                                        B2 = (x + c) + " " + (y - 1);
+                                    }
+                                }
+                                catch { }
+
+                                try
+                                {
+                                    if (P[x + 1, y].Image == null)
+                                    {
+                                        P[x + 1, y].Image = Properties.Resources.b;
+                                        P[x + 1, y].Name = (x + 1) + " " + (y) + " b";
+                                        B3 = (x + 1) + " " + (y);
+                                    }
+                                }
+                                catch { }
+
+                                try
+                                {
+                                    if (P[x - 1, y].Image == null)
+                                    {
+                                        P[x - 1, y].Image = Properties.Resources.b;
+                                        P[x - 1, y].Name = (x - 1) + " " + (y) + " b";
+                                        B4 = (x - 1) + " " + (y);
+                                    }
+                                }
+                                catch { }
+                            }
+                            #endregion
+                        }
+                    };
+                    G.Controls.Add(P[i, j]);
+                }
+                top += 60;
+            }
+        }
+        public void F()
+        {
+            // B1, B2, B3 ve B4 olarak adlandırılan taşa tıklandıktan sonra etrafında çıkan gidilebilir alanlar.
+            if (B1 != " ")
+            {
+                int x, y;
+                x = Convert.ToInt32(B1.Split(' ')[0]);
+                y = Convert.ToInt32(B1.Split(' ')[1]);
+                P[x, y].Image = null;
+            }
+
+            if (B2 != " ")
+            {
+                int x, y;
+                x = Convert.ToInt32(B2.Split(' ')[0]);
+                y = Convert.ToInt32(B2.Split(' ')[1]);
+                P[x, y].Image = null;
+            }
+
+            if (B3 != " ")
+            {
+                int x, y;
+                x = Convert.ToInt32(B3.Split(' ')[0]);
+                y = Convert.ToInt32(B3.Split(' ')[1]);
+                P[x, y].Image = null;
+            }
+
+            if (B4 != " ")
+            {
+                int x, y;
+                x = Convert.ToInt32(B4.Split(' ')[0]);
+                y = Convert.ToInt32(B4.Split(' ')[1]);
+                P[x, y].Image = null;
+            }
+        }
+    }
+}
